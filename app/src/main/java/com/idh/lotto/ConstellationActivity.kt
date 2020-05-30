d01c4abcb484599dcad51ba60b1e4d3d8b65ae53
songkho <songkho12@naver.com>
2020-05-30 23:06:34 +0900

    로또_1

---
+++ app/src/main/java/com/idh/lotto/ConstellationActivity.kt
@@ -0,0 +1,12 @@
+package com.idh.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+    }
+}
